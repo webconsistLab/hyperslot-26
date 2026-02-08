cf368b6b1b3c795a202c13c076e2bafc8170e9d2
WebConsist <122109601+webconsistLab@users.noreply.github.com>
2026-02-09 01:58:37 +0600

    Create signin-fetch.js

---
+++ signin-fetch.js
@@ -0,0 +1,73 @@
+fetch("https://api.ivacbd.com/iams/api/v1/auth/signin", {
+  "headers": {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "en-US,en;q=0.9,bn;q=0.8,de;q=0.7,es;q=0.6",
+    "cache-control": "no-cache, no-store, must-revalidate",
+    "content-type": "application/json",
+    "pragma": "no-cache",
+    "priority": "u=1, i",
+    "sec-ch-ua": "\"Not(A:Brand\";v=\"8\", \"Chromium\";v=\"144\", \"Google Chrome\";v=\"144\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site"
+  },
+  "referrer": "https://appointment.ivacbd.com/",
+  "body": "{\"phone\":\"01764806080\",\"password\":\"Msd@458@\",\"captchaToken\":\"0cAFcWeA5TIRKoqhJ0XdEWcldIPLn8BQSKQYE2k1dHUkudLTq0cVSbG8B5H3SsjCmVhhPUsIp9hGvP3hVuS6vQgDRHdNA3u5WkJ_mfmI6XncqyLDbnUDgw0dS_DREfJd_fj5BnX5gWdMoNUrC5gqBK-Ia820U7HmdHwAD9sxJnVsgVYgE-jttLF9xP7Ffe2WZAFA63bOPpb9WLJLOxu75dIOECzojyk6uwQigdGM9FabxGEVEhjdN2ydl6w-hY3GnCSdgT1K5XEntjDi9B6xQQ4nSh9DTYkzOXq1nk-hWDgSXUIvqUTorxs5T_uG0pd87qmgNoNqy_mTJMPlNLPtZZk6-nPcEB2AzLQZZ2YI4fnrYqCG7MrJrZPZyNv7zRTldw5TFulujcOvCMOrS7ZrLoFjaWeVt_V1Qz9LZiVGKL-WFavcT3ddKyb2PA6jdqXjyJpq2pa6VJUCOoMgynJqzVF3AISSo5WDDbSbQSmOy9BO5o0eEE0C55WhgtZLvLzhoRpm-qpXoJivIduh06K1tEF1sNdaqfrcatKtqybo1w0pC8kqjWtWGOP_E632S7QPBJ8s9iqVrfQoaAL8x__V_vxHwgwa9NQJTL-p4mcZhEmJR8xf6jKgXFzcmYjSFL3HlNlXkkbleO6UCIAFiDzQFLyONTaB6RJbN4KtZk0Kl1RwWCk6_63AvUhTSU3mh06kG94b-RgC4QVHv0x3MsqnH0WP4IX5OLWlJIGGqir5s53dUtSfIs9REyxMTUYOWET2ViPKTBSr0H0_gPLda4lwjOmA3HaSSl0ucNLHZii65DZZHuhHhsl-IzM2jdqOzTmPAoBvXHLz6szKNPH7mG-gyYgOFdHsICHAtrhrKj3m1WhaBmARwov58C2aMhqzBhejTWyaETviGo3c6EsWOPBW43zUwx3vmQRJOjdoz8LU8lEsv5r0CxdvvBITREr3dyUuYgrLDfBFTABxZTkLOwMSwGLhVxhcAINWLRP8po6Mtftv9aSDE516790KvUnWt59DU73W7jlaON2jZ9KEWi2hOs2G5cjRyvwJtxR5eEHOsCpZZNgpqGIi9-4MxyYgMboTr_6jNfUxVDd59yzcPRcvZvM9cfRfkP-V9RCZ3EfyKIHdAv5uJlo35b3T2ImwYZiJv2opFMT_bhAAp0YEjtlXypoLiDfvkZoO6ScK2EvodHShBPwaU09AhpW_iLWOrjp-KUu-TzoLGFjpyUKpr4NtD3cEKyfXpr55IxEaXRHm46ZLov7w__c5ITgjiqUc869OPSToCEzrqNwK86twt-1kBN5bdVklAJ-dysKHw8DnJskzB9JFPXEFfHiLy2scuAZTNr3mhNUIvSIXUWZtmveNsCT9j3W_3g4GP0x1c30s4DQsTEMrptWMcZBeIMAJR2aonw2W9M7SPFjcn1E0Ev2v3c0K-nQJXhMKxiEQtCkBb2NwbTIHxUIrxB9uWE-eIE3t94vkKKq1hUlR3o9FDR6WXHEVlIJPOIlw3nFNzz6hd4vOfIrelXCQAUYNwhIrHkLLZGxzoQw5FsFwnrZuHp5FCYoaeE75VLhAfhdWZkrTdgKJPJX5G0vPO0phw-C1xXNuU-HNr5S4r_1EpGN7DGWXYnjG2svaGOGF8-bXJ-aSHShw4PDjAX5lbnaunTh9yXYgwmEBicXcGwHeKBdfPn8ITGxbKn8zxIc5ifiHutndM9ve7v2ZB60g2NupXsl_Q9fP9qbhh4WiW63epdlDR3tFSmK-AdqobIDU1FJjmtMjRbBiH5_7C8DTAQ1roKwOmlTmbukvdlO9y1AAhU2daKm25iLIec5PNUvDx3uNv8S1ZpJ0U8pfR7y7m7dvDUEHEJ5rj2JaMYtly0ne68tMX4Sp48l7f9dzw7IE2YFmjY83z7VwNIVZoozFiM4ZNiuppJewnwYtzTT4Kn6-YpRB3HO_ynzBdzL6R5qxf9Hd5LenvBKnlTgvwYu2HL3bxbXAQLt13ayOrdWpdDYqYpmjhCByuHesLRcx0ilYnSrXZvv2LO3QVN4PoKRNMlQOpRrRtr9Vc70r4sFvcHaoSaHRJDbBs0mSxxaafLByO7YwUQNj9wX-1IIPYLydlZuXrW3e8MMxsGVuY0Qm2__M-1S4ubV3yjNgL9feBoX3NVTZBCngWGzzqnNjjjcOvuVq4\"}",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "omit"
+});
+
+
+{
+    "data": {
+        "accessToken": "eyJraWQiOiJpYW0tand0LXJzYS0xIiwidHlwIjoiSldUIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiI3OGUzNDM1Zi1mYmNkLTQ2NzktYWFmYy0zYWM3YjhlMGNlZjQiLCJhdWQiOiJpYW1zLWFwaSIsInJvbGVzIjpbXSwiaXNzIjoiaWFtcyIsImV4cCI6MTc3MDU4MTMyMSwiaWF0IjoxNzcwNTgwNDIxLCJqdGkiOiJlOWI3NzJjOC05MDZiLTQ4ZTMtODgwMC03OTVkMjhlZDQyYzAiLCJzaWQiOiJjYTE3NDU2YS1mYzliLTQ3OTMtOTJhNC04MzI4NWRiNDY0ZDIifQ.E-ARZvapW7OlJ63E89v4jrOfnlYggRa-eWOk_jdAnQoVhQsybJODETLoGlWrIYA_yRdc2akl9_eTXpMMhtc3mwfJsO4t7CHwrcJBzS8-ruNI10CsWN3WFX2A-9K4juwVzt9Ni8XdY60p6t9J7wY_R6YPcynM0coxrtpxmVFxiALDRgDWrcZRqQx1xgFibmn334yU_bnTpf-7ZvaMezUFLEBy91FcsnQ_Ux0J5Dhqq5qWJEeCj71l42y_RLu183REtxI2WlBBV8anNWqt5k-pMvOoSocc24o4VMAoiWaZohBFH36_Lm8UJ3848YLcDWqY1BtyMG--XFDCj4VscV9ldQ",
+        "tokenType": "Bearer",
+        "expiresAt": 899,
+        "userId": "78e3435f-fbcd-4679-aafc-3ac7b8e0cef4",
+        "requestId": "5867bf48-597f-43e1-a7c6-d4caad21ab96",
+        "roles": []
+    },
+    "statusCode": 200,
+    "message": "Success",
+    "successFlag": true,
+    "serverTime": "2026-02-08T19:53:41.846319619Z"
+}
+
+
+fetch("https://api.ivacbd.com/iams/api/v1/otp/verifySigninOtp", {
+  "headers": {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "en-US,en;q=0.9,bn;q=0.8,de;q=0.7,es;q=0.6",
+    "authorization": "Bearer eyJraWQiOiJpYW0tand0LXJzYS0xIiwidHlwIjoiSldUIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiI3OGUzNDM1Zi1mYmNkLTQ2NzktYWFmYy0zYWM3YjhlMGNlZjQiLCJhdWQiOiJpYW1zLWFwaSIsInJvbGVzIjpbXSwiaXNzIjoiaWFtcyIsImV4cCI6MTc3MDU4MTMyMSwiaWF0IjoxNzcwNTgwNDIxLCJqdGkiOiJlOWI3NzJjOC05MDZiLTQ4ZTMtODgwMC03OTVkMjhlZDQyYzAiLCJzaWQiOiJjYTE3NDU2YS1mYzliLTQ3OTMtOTJhNC04MzI4NWRiNDY0ZDIifQ.E-ARZvapW7OlJ63E89v4jrOfnlYggRa-eWOk_jdAnQoVhQsybJODETLoGlWrIYA_yRdc2akl9_eTXpMMhtc3mwfJsO4t7CHwrcJBzS8-ruNI10CsWN3WFX2A-9K4juwVzt9Ni8XdY60p6t9J7wY_R6YPcynM0coxrtpxmVFxiALDRgDWrcZRqQx1xgFibmn334yU_bnTpf-7ZvaMezUFLEBy91FcsnQ_Ux0J5Dhqq5qWJEeCj71l42y_RLu183REtxI2WlBBV8anNWqt5k-pMvOoSocc24o4VMAoiWaZohBFH36_Lm8UJ3848YLcDWqY1BtyMG--XFDCj4VscV9ldQ",
+    "cache-control": "no-cache, no-store, must-revalidate",
+    "content-type": "application/json",
+    "pragma": "no-cache",
+    "priority": "u=1, i",
+    "sec-ch-ua": "\"Not(A:Brand\";v=\"8\", \"Chromium\";v=\"144\", \"Google Chrome\";v=\"144\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site"
+  },
+  "referrer": "https://appointment.ivacbd.com/",
+  "body": "{\"requestId\":\"5867bf48-597f-43e1-a7c6-d4caad21ab96\",\"phone\":\"01764806080\",\"code\":\"092381\",\"otpChannel\":\"PHONE\"}",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "include"
+});
+
+{
+    "data": {
+        "verified": true,
+        "verificationStatus": "OTP verified",
+        "expiresAt": "2026-02-08T19:58:41.761813Z"
+    },
+    "statusCode": 200,
+    "message": "Success",
+    "successFlag": true,
+    "serverTime": "2026-02-08T19:54:08.047191128Z"
+}
